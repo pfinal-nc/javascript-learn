235b61a98e41250d078e321b47f10956e649be8e
PFinal_南丞 <1370288056@qq.com>
2024-01-15 19:08:34 +0800

    2024-01-15 up

--- TypeScript/symbol_ts.ts
+++ TypeScript/symbol_ts.ts
@@ -16,3 +16,20 @@ const st1:unique symbol = Symbol();
 // 报错
 const st2:unique symbol = Symbol();
 
+// unique symbol
+// symbol 类型包含所有的 Symbol 值,但是无法表示某一个具体的 Symbol 值. 比如，5是一个具体的数值，就用5这个字面量来表示，这也是它的值类型。但是，Symbol 值不存在字面量，必须通过变量来引用，所以写不出只包含单个 Symbol 值的那种值类型。但是，Symbol 值不存在字面量，必须通过变量来引用，所以写不出只包含单个 Symbol 值的那种值类型。
+
+const st3:unique symbol = Symbol();
+
+// 上面示例中, let 命令声明的变量, 不能是 unique symbol 类型,const 命令为变量赋值 symbol 值时，变量类型默认就是unique symbol，所以类型可以省略不写。所以类型可以省略不写
+
+const st4:unique symbol = Symbol();
+
+
+// 类型判断
+// 如果变量声明时没有给出类型, TypeScript 会推单某个 Symbol 值变量的类型
+// let 命令声明的变量, 推断类型为 symbol.
+// 类型为 symbol
+
+let sx = Symbol();
+console.log(sx)
