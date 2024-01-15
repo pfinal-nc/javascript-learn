89795f6c61c9b03ec71aa2917a44e6dfdcedffec
PFinal_南丞 <1370288056@qq.com>
2024-01-15 16:26:01 +0800

    2024-01-15 up

--- TypeScript/array_ts.ts
+++ TypeScript/array_ts.ts
@@ -14,5 +14,34 @@
 // arr = [1, 2, 3];
 
 // 这种规定的隐藏含义就是, 数组的成员是可以动态变化的
+// let arr:number[] = [1, 2, 3];
+// arr[3] = 4;
+// arr.length = 2;
+// console.log(arr)
+// 上面示例中, 数组增加成员或减少成员,都是可以的 正式由于成员数量可以动态变化, 所以 TppeScript 不会对数组边界进行检查, 越界访问数组并不会报错
 let arr:number[] = [1, 2, 3];
-arr[3] = 4;
+let foo = arr[3]; // 正确 变量 foo 的值是一个不存在的数组成员, TypeScript 并不会报错 TypeScript 允许使用方括号读取数组成员的类型
+
+// type Names = string[];
+// type Name = Names[0]; // string
+
+// 类型 Names 是字符串数组, 那么 Names[0] 返回的类型就是 string. 由于数组成员的索引类型都是 number 所以读取成员类型也可以写成下面这样
+type Names = string[];
+type Name = Names[number]; // string
+
+// 上面示例中, Names[number] 表示数组 Names 所有数值索引的成员类型, 所以返回 string
+
+// 数组的类型推断
+// 如果数组变量没有声明类型, TypeScript 就会推断数组成员的类型, 推断行为会因为值的不同,而有所不同. 如果变量的初始值是空数组, TypeScript 会推断数组类型是 any[]
+
+const array:(string|number)[] = []; // 为这个数组赋值时, TS 会自动更新类型推断
+array.push(1);
+array.push("abc");
+
+console.log(array)
+
+// 多维数组
+
+var multi:number[][] = [[1, 2, 3], [4, 5, 6]];
+
+console.log(multi[0][1]) // 2
