86e44bd41f3b9266f4aeb00997eea776a40eebd2
PFinal_南丞 <1370288056@qq.com>
2024-01-31 14:30:08 +0800

    2024-01-31 更新

--- TypeScript/type_ts.ts
+++ TypeScript/type_ts.ts
@@ -1,1 +1,7 @@
-// 使用js 不能自定义类型
+// 使用js 不能自定义类型 使用 ts 就可以
+
+function getTitle(name: 'PFinalClub' | 'PFinal') { 
+    return name === 'PFinal' ? 'PFinal' : 'PFinalClub';
+}
+
+getTitle('PFinal'); // 输出 "PFinal"
