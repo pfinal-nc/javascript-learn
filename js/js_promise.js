3c8e8d018086f3a186bf7348045af72b51bf0814
PFinal_南丞 <1370288056@qq.com>
2024-02-05 11:39:16 +0800

    2024-02-05 up

--- js/js_promise.js
+++ js/js_promise.js
@@ -139,3 +139,23 @@ promise_t.then(function(value) { console.log(value) }).catch(function(error) { c
  * 
  *  Promise.prototype.finally()
  */
+// finally()方法用于指定不管 Promise 对象最后状态如何，都会执行的操作
+// promise.then(result => {···})
+// .catch(error => {···})
+// .finally(() => {···});
+
+// Promise.all()
+// Promise.all() 方法用于将多个 Promise 实例, 包装成一个新的 Promise 实例
+
+// const p = Promise.all([p1, p2, p3]);
+// 上面代码中, Promise.all() 方法接受一个数组作为参数, p1,p2,p3 都是promise实例. 如果不是, 就会先调用 Promise.resolve 方法, 将参数转为 Promise 实例, 再进一步处理.
+// 另外, promise.all() 方法的参数可以不是数组,但必须具有 iterator 接口, 且返回的每个成员都是 promise 实例
+// p 的状态 由 p1,p2,p3 决定, 分成两种情况
+// 1) 只有p1,p2,p3的状态都变成 fuifilled,p的状态才会变成 fulfilled，此时p1,p2,p3的返回值组成一个数组, 传递给p的回调函数.
+// 2)  只要p1,p2,p3之中有一个被 rejected, p的状态就变成 rejected 此时第一个被 reject的实例的返回值, 会传递给 p 的回调函数.
+
+
+
+
+
+
