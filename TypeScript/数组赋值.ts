5bd21c4706003d39570ae9fc847506bb91ed1c0d
PFinal_南丞 <1370288056@qq.com>
2024-03-08 17:13:45 +0800

    update

---
+++ TypeScript/数组赋值.ts
@@ -0,0 +1,13 @@
+let pa = 'pfinal';
+let pf1 = 2024;
+// let pc = [pa,pf1];
+// let pfina = pc[1]
+// console.log(pfina)
+// pfina = "PF"
+// console.log(pfina)
+
+let pc = <const>[pa,pf1];
+let pfina = pc[1]
+console.log(pfina)
+pfina = 2023
+console.log(pfina)
