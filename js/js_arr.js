a79eed278cdf026f4fa8e1885d83d1ed902518a0
pfinal <lampxiezi@163.com>
2024-07-16 21:35:54 +0800

    update

---
+++ js/js_arr.js
@@ -0,0 +1,9 @@
+// 数组
+console.log(new Array(1,'PFinalClub','pfinal'));
+
+const array = ["pfinal", "pfinalclub"];
+
+console.log(array);
+
+const array1 = [["PFinalClub"], ["南丞"]];
+console.log(array1[1][0]);
