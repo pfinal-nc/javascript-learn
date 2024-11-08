079883994025932722adf9e7ea286b000f664935
PFinal_南丞 <1370288056@qq.com>
2024-11-08 10:27:41 +0800

    int

--- js/js_迭代.js
+++ js/js_迭代.js
@@ -15,3 +15,26 @@ console.log(sequence);
 
 
 // 使用 generator 进行迭代
+
+let range = {
+    form :1,
+    to : 5,
+    // for ..of rang 在一开始 就调用一次这个方法
+    [Symbol.iterator]() {
+        return {
+            current: this.form,
+            last: this.to,
+            next() {
+                if(this.current <= this.last) {
+                    return {done: false, value: this.current++};
+                } else {
+                    return {done: true};
+                }
+            }
+        }
+    }
+}
+
+/// 迭代整个 rang 对象 返回从 range.form 到 range.to 的数字
+
+console.log([...range]);
